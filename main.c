e0deb3747fc85450cd9fb1be0e249bf4bfde345d
Arda Cinar <kuzux92@gmail.com>
2018-01-15 19:11:26 +0300

    Some refactoring work

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,33 +12,93 @@
 
 #define BUFSIZE 4096
 
-size_t generate_code(size_t buflen, uint8_t* buf, size_t numslen, int32_t* nums) {
+#define DEBUG
+
+#ifdef DEBUG
+#define TRACE(x) { fprintf(stderr, "%s\n", (x)); }
+#else
+#define TRACE /* TRACE no-op */
+#endif
+
+typedef struct {
+    uint8_t* buf;
+    size_t buflen;
+} buffer_t;
+
+typedef struct {
+    int32_t* data;
+    size_t len;
+    size_t allocated;
+} int32_vec_t;
+
+bool alloc_buffer(buffer_t* buf, size_t len) {
+    buf->buf = (uint8_t*)malloc(len);
+    if(!buf->buf) {
+        // malloc failed
+        buf->buflen = 0;
+        return false;
+    } else {
+        buf->buflen = len;
+        return true;
+    }
+}
+
+bool buffer_checked_at(buffer_t* buf, size_t idx, uint8_t* res) {
+    if(idx <= buf->buflen) {
+        *res = buf->buf[idx];
+        return true;
+    } else {
+        res = NULL;
+        return false;
+    }
+}
+
+uint8_t buffer_unchecked_at(buffer_t* buf, size_t idx) {
+    return buf->buf[idx];
+}
+
+bool buffer_checked_put(buffer_t* buf, size_t idx, uint8_t elem) {
+    if(idx <= buf->buflen) {
+        buf->buf[idx] = elem;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void free_buffer(buffer_t* buf) {
+    free(buf->buf);
+    buf->buf = NULL;
+    buf->buflen = 0;
+}
+
+size_t generate_code(buffer_t* buf, size_t numslen, int32_t* nums) {
     size_t pos = 0;
-    for(size_t i=0; i<numslen-1 && pos<buflen ;i++) {
+    for(size_t i=0; i<numslen-1 && pos<buf->buflen; i++) {
         // mov eax
-        buf[pos++] = 0xb8;
+        buffer_checked_put(buf, pos++, 0xb8);
 
         // put in that number
-        buf[pos++] = (nums[i] & 0x000000FF);
-        buf[pos++] = (nums[i] & 0x0000FF00) >> 8;
-        buf[pos++] = (nums[i] & 0x00FF0000) >> 16;
-        buf[pos++] = (nums[i] & 0xFF000000) >> 24;
+        buffer_checked_put(buf, pos++, (nums[i] & 0x000000FF));
+        buffer_checked_put(buf, pos++, (nums[i] & 0x0000FF00) >> 8);
+        buffer_checked_put(buf, pos++, (nums[i] & 0x00FF0000) >> 16);
+        buffer_checked_put(buf, pos++, (nums[i] & 0xFF000000) >> 24);
 
         // mov ebx
-        buf[pos++] = 0xbb;
+        buffer_checked_put(buf, pos++, 0xbb);
 
         // put in that number
-        buf[pos++] = (nums[i+1] & 0x000000FF);
-        buf[pos++] = (nums[i+1] & 0x0000FF00) >> 8;
-        buf[pos++] = (nums[i+1] & 0x00FF0000) >> 16;
-        buf[pos++] = (nums[i+1] & 0xFF000000) >> 24;
+        buffer_checked_put(buf, pos++, (nums[i] & 0x000000FF));
+        buffer_checked_put(buf, pos++, (nums[i] & 0x0000FF00) >> 8);
+        buffer_checked_put(buf, pos++, (nums[i] & 0x00FF0000) >> 16);
+        buffer_checked_put(buf, pos++, (nums[i] & 0xFF000000) >> 24);
 
         // add eax, ebx
-        buf[pos++] = 0x01;
-        buf[pos++] = 0xd8;
+        buffer_checked_put(buf, pos++, 0x01);
+        buffer_checked_put(buf, pos++, 0xd8);
     }
 
-    buf[pos++] = 0xc3;
+    buffer_checked_put(buf, pos++, 0xc3);
 
     return pos;
 }
@@ -56,10 +117,10 @@ size_t page_aligned_length(size_t len) {
     return res;
 }
 
-void print_code(size_t codelen, uint8_t* code) {
-    for(size_t i=0; i<codelen; i++) {
-        printf("%02x", code[i]);
-        if(i%8 != 7){
+void print_code(buffer_t* buf, int len) {
+    for(size_t i=0; i<len; i++) {
+        printf("%02x", buffer_unchecked_at(buf, i));
+        if(i%8 != 7) {
             putchar(' ');
         } else {
             putchar('\n');
@@ -70,20 +131,30 @@ void print_code(size_t codelen, uint8_t* code) {
 }
 
 int main(int argc, char* argv) {
-    printf("test\n");
-    printf("%d\n", sysconf(_SC_PAGE_SIZE));
+    TRACE("starting up");
 
-    uint8_t* code_buf = (uint8_t*)malloc(BUFSIZE);
+    buffer_t* code_buf;
+    code_buf = (buffer_t*)malloc(sizeof(buffer_t));
     if(!code_buf) {
+        panic("malloc failed");
+    }
+
+    if(!alloc_buffer(code_buf, BUFSIZE)) {
         panic("Couldn't allocate memory");
     }
 
+    TRACE("generating code");
+
     int32_t nums[4] = {3,5,7,9};
-    uint32_t code_len = generate_code(BUFSIZE, code_buf, 4, nums);
+    uint32_t code_len = generate_code(code_buf, 4, nums);
     
-    print_code(code_len, code_buf);
+#ifdef DEBUG
+    print_code(code_buf, code_len);
+#endif
 
     size_t exec_len = page_aligned_length(code_len);
+
+    TRACE("generated code");
 
     uint8_t* exec_mem = (uint8_t*)mmap(NULL, exec_len, 
             PROT_READ | PROT_WRITE | PROT_EXEC, 
@@ -92,7 +163,9 @@ int main(int argc, char* argv) {
         panic("Couldn't mmap()");
     }
 
-    memcpy(exec_mem, code_buf, code_len);
+    TRACE("mmap done");
+
+    memcpy(exec_mem, code_buf->buf, code_len);
 
     int (*fn)();
     fn = (int (*)())exec_mem;
@@ -101,6 +174,9 @@ int main(int argc, char* argv) {
     printf("result = %d\n", res);
 
     munmap(exec_mem, exec_len);
+
+    free_buffer(code_buf);
+    free(code_buf);
 
     return 0;
 }
